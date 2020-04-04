d0c8669532441b4b20b9e38b6123bd6ea3fd9907
Ryan Waldon <ryanpwaldon@gmail.com>
2020-04-04 18:57:59 +1100

    updates

--- src/views/App/views/Listing/Listing.vue
+++ src/views/App/views/Listing/Listing.vue
@@ -37,7 +37,7 @@
       </BaseLayoutCenter>
       <div class="map-container">
         <BaseMap :center="listing.lngLat" :zoom="16">
-          <Pin :lng-lat="listing.lngLat"/>
+          <Markers :lng-lats="[listing.lngLat]"/>
         </BaseMap>
       </div>
     </div>
@@ -52,7 +52,7 @@ import BaseText4 from '@/components/BaseText4/BaseText4'
 import BaseText5 from '@/components/BaseText5/BaseText5'
 import BaseGallery from '@/components/BaseGallery/BaseGallery'
 import BaseMap from '@/components/BaseMap/BaseMap'
-import Pin from '@/components/BaseMap/components/Pin/Pin'
+import Markers from '@/components/BaseMap/components/Markers/Markers'
 import Contact from './partials/Contact/Contact'
 import ListingService from '@/services/Api/services/ListingService/ListingService'
 export default {
@@ -64,7 +64,7 @@ export default {
     BaseText5,
     BaseGallery,
     BaseMap,
-    Pin,
+    Markers,
     Contact
   },
   props: {
