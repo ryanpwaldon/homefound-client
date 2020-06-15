3868819587118499ffdd0dffa5ed22b72743a503
Ryan Waldon <ryanpwaldon@gmail.com>
2020-06-15 14:01:07 +1000

    updates

--- src/views/Home/partials/Features/Features.vue
+++ src/views/Home/partials/Features/Features.vue
@@ -15,6 +15,11 @@ export default {
         title: 'How it works',
         paragraph: 'Real estate agents use Homefound to get <span style="white-space: nowrap">off-market</span> properties in front of motivated buyers. Buyers subscribe to gain access.'
       },
+      // {
+      //   icon: require('@/assets/img/search-circle.svg'),
+      //   title: 'Focus on your client',
+      //   paragraph: `Use Homefound to automate your <span style="white-space: nowrap">off-market</span> property discovery so you can focus on what matters most.`
+      // }
       {
         icon: require('@/assets/img/search-circle.svg'),
         title: 'Save time. Discover more.',
